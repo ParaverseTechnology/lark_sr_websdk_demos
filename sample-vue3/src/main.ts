9a2f1d623163c4e174f57f4054a9342ae2c18a99
fcx <fcx@pingxingyun.com>
2022-05-07 17:07:53 +0800

    change vue3 sample to typescript.

---
+++ sample-vue3/src/main.ts
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
+
+// 在 typescript 中引入方式
+//
+// import Lark from "larksr_websdk";
+// const client = new Lark.LarkSR({ 
+    // rootElement: '', 
+    // serverAddress: ''
+// });
